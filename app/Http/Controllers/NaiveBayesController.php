389945eb95fd84d31dbf0dd23f362281979a025a
sp <suhar.tyo@gmail.com>
2017-06-05 17:06:58 +0700

    naive bayes

--- app/Http/Controllers/NaiveBayesController.php
+++ app/Http/Controllers/NaiveBayesController.php
@@ -7,11 +7,9 @@ use Illuminate\Http\Request;
 class NaiveBayesController extends Controller
 {
     public function conditionalProbability(){
-    	$nb_conditional_probabilities = \App\NbConditionalProbability::where('system_id', \Session::get('SYSTEM_ID'))->orderBy('id', 'asc')->get();
-    	$attributes = \App\Attribute::where('system_id', \Session::get('SYSTEM_ID'))->orderBy('id', 'asc')->get();
-        $data_classes = \App\DataClass::where('system_id', \Session::get('SYSTEM_ID'))->orderBy('id', 'asc')->get();
-
-    	return view('naive-bayes.index', compact('nb_conditional_probabilities', 'attributes', 'data_classes'));
+    	$nb_conditional_probabilities = \App\NbConditionalProbability::where('system_id', \Session::get('SYSTEM_ID'))->orderBy('data_class_id', 'asc')->get();
+    	
+    	return view('naive-bayes.index', compact('nb_conditional_probabilities'));
     }
 
     public function train(Request $request){
@@ -20,11 +18,47 @@ class NaiveBayesController extends Controller
 
     	// Using Bernouli
     	$data_classes = \App\DataClass::where('system_id', \Session::get('SYSTEM_ID'))->get();
-    	$matrix_datasets = \App\MatrixDataset::where('system_id', \Session::get('SYSTEM_ID'))->get();
+    	$attributes = \App\Attribute::where('system_id', \Session::get('SYSTEM_ID'))->get();
+    	$datasets = \App\Dataset::where('system_id', \Session::get('SYSTEM_ID'))->get();
+
+    	// N = Jumlah keseluruhan Dataset
+    	$N = $datasets->count();
+
+    	// M = Jumlah keseluruhan atribut
+    	$M = $attributes->count();
+
     	foreach ($data_classes as $data_class) {
-    		// Peluang class tertentu pada dataset
-    		$Pj = 
+    		// ni = Jumlah class i pada datasets
+    		$n = $data_class->datasets->count();
+
+    		// P(Hi) = ni/N = peluang setiap class i pada dataset
+    		$PH = $n/$N;
+
+    		foreach ($attributes as $attribute) {
+    			$nb_conditional_probability = new \App\NbConditionalProbability;
+    			$nb_conditional_probability->system_id = \Session::get('SYSTEM_ID');
+    			$nb_conditional_probability->data_class_id = $data_class->id;
+    			$nb_conditional_probability->attribute_id = $attribute->id;
+
+    			// data atribut j yang muncul pada class i dalam dataset
+    			$matrix_datasetsNc = \App\MatrixDataset::where('data_class_id', $data_class->id)
+    													->where('attribute_id', $attribute->id)
+    													->get();
+
+    			// ncji = jumlah atribut j yang muncul pada class i dalam dataset
+    			$nc = $matrix_datasetsNc->count();
+    			
+    			// P(Ej|Hi) = conditional probability
+    			// tanpa Smoothing = P(Ej|Hi) = ncji/n
+    			$PEH = $nc/$n;
+
+    			$nb_conditional_probability->value = $PEH;
+    			$nb_conditional_probability->save();
+    		}
+
     	}
+
+    	return redirect('naive-bayes')->with('status', 'Train data berhasil');
 
     }
 
--- resources/views/naive-bayes/index.blade.php
+++ resources/views/naive-bayes/index.blade.php
@@ -1,66 +1,34 @@
 @extends('layouts.app')
 
 @section('title')
-	Daftar Sistem
+	Conditional Probabilities
 @endsection
 
 @section('content')
 		
 	<script>
 		init.push(function () {
-			$('#jq-datatables-systems').dataTable();
-	        $('#jq-datatables-systems_wrapper .table-caption').text('Daftar Sistem');
-	        $('#jq-datatables-systems_wrapper .dataTables_filter input').attr('placeholder', 'Cari...');
+			$('#jq-datatables-naive-bayes-conditional-probabilities').dataTable();
+	        $('#jq-datatables-naive-bayes-conditional-probabilities_wrapper .table-caption').text('Daftar Nilai');
+	        $('#jq-datatables-naive-bayes-conditional-probabilities_wrapper .dataTables_filter input').attr('placeholder', 'Cari...');
 
 	        $('.list-item').popover();
 	        $('.list-item').on('click', ':not(a)', function (e) {
 			    $('.list-item').not(this).popover('hide');
 			});
 
-			$('.switcher-systems').switcher({
-				theme: 'square',
-				on_state_content: 'Aktif',
-				off_state_content: 'Tidak'
-			});
-
-			$('.switcher-systems').on('click', function(){
-				containerOverlay();
-				if(this.checked){
-					window.location.href = '/change-system/'+ $(this).parent().parent().parent('.list-item').attr('data-id');
-				} else {
-					window.location.href = '/change-system/0';
-				}
-			});
-
 		});
-
-		function deleteSystem(index){
-			var id = $(index).parent().parent('.list-item').attr('data-id');
-        	bootbox.confirm({
-                message: "Anda yakin ingin menghapus data?",
-                callback: function(result) {
-                    if(result){
-                        containerOverlay();
-			    		var form = $('#form-systems-delete');
-			    		$(form).attr('action','/systems/'+ id);
-			    		$(form).submit();
-                    }
-                },
-                className: "bootbox-sm"
-            });
-            return false;
-		}
 	</script>
 
 	<ul class="breadcrumb breadcrumb-page">
 		<div class="breadcrumb-label text-light-gray">You are here: </div>
-		<li><a href="javascript::void()">Sistem</a></li>
-		<li class="active"><a href="javascript::void()">Daftar</a></li>
+		<li><a href="javascript::void()">Naive Bayes</a></li>
+		<li class="active"><a href="javascript::void()">Train</a></li>
 	</ul>
 
 	<div class="panel">
 	    <div class="panel-heading">
-	        <span class="panel-title">Sistem</span>
+	        <span class="panel-title">Atribut</span>
 	    </div>
 		<div class="panel-body">
 			@if (session('status'))
@@ -68,35 +36,33 @@
 			        {{ session('status') }}
 			    </div>
 			@endif
-			<form class="form-horizontal" id="form-systems-delete" action="/systems/" method="post">
-			    {{ csrf_field() }}
-			    <input type="hidden" name="_method" value="DELETE">
-			</form>
 	        <div class="table-primary">
-	            <div class="form-group">
-	                <a href="/systems/create" class="btn btn-flat btn-labeled btn-primary"><span class="btn-label icon fa fa-plus"></span>Buat Sistem Baru</a>
-	            </div>
+	        	<form action="/naive-bayes/train" method="POST">
+	        		{{ csrf_field() }}
+		            <div class="form-group">
+		                <button class="btn btn-flat btn-labeled btn-primary"><span class="btn-label icon fa fa-plus"></span>Train Dataset</button>
+		            </div>
+	        	</form>
 	            <div>
-	            	<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="jq-datatables-systems">
+	            	<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="jq-datatables-naive-bayes-conditional-probabilities">
 						<thead>
 							<tr>
 								<th width="10px">No</th>
-								<th>Nama</th>
-								<th>Status</th>
+								<th>Class</th>
+								<th>Atribut</th>
+								<th>Nilai</th>
 								<th class="text-center" width="120px">Aksi</th>
 							</tr>
 						</thead>
 						<tbody>
-						@foreach ($systems as $system)
-							<tr class="list-item" data-id="{{ $system->id }}" data-toggle="popover" data-placement="bottom" data-content="{{ $system->description }}" data-title="{{ $system->name }}" data-original-title="" title="{{ $system->name }}">
+						@foreach ($nb_conditional_probabilities as $nb_conditional_probability)
+							<tr class="list-item" data-id="{{ $nb_conditional_probability->id }}" data-toggle="popover" data-placement="bottom" data-content="{{ $nb_conditional_probability->free }}" data-title="{{ $nb_conditional_probability->name }}" data-original-title="" title="{{ $nb_conditional_probability->name }}">
 								<td>{{ $loop->iteration }}</td>
-								<td>{{ $system->name }}</td>
+								<td>{{ $nb_conditional_probability->data_class->name }}</td>
+								<td>{{ $nb_conditional_probability->attribute->name }}</td>
+								<td>{{ $nb_conditional_probability->value }}</td>
 								<td>
-									<input type="checkbox" data-class="switcher-success" class="switcher-systems" {{ Session::get('SYSTEM_ID')==$system->id ? 'checked="checked"': '' }} >
-								</td>
-								<td>
-									<a href="/systems/{{ $system->id }}/edit" class="btn btn-flat btn-sm btn-warning" title="Ubah"><span class="btn-label icon fa fa-edit"></span></a>
-									<a href="#" class="btn btn-flat btn-sm btn-danger btn-delete" title="Hapus" onclick="return deleteSystem(this)"><span class="btn-label icon fa fa-trash"></span></a>
+									<a href="naive-bayes/{{ $nb_conditional_probability->id }}/edit" class="btn btn-flat btn-sm btn-warning" title="Ubah"><span class="btn-label icon fa fa-edit"></span></a>
 								</td>
 							</tr>
 						@endforeach
